aaac222e2b2db908fd125fdf45c9631862ff74dc
*Tiago <tiagomoreira3f@gmail.com>
2021-08-10 11:21:34 -0300

    feat: :sparkles: Http with Axios

---
+++ http/http-exercicios/src/plugins/axios.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+
+// axios.defaults.baseURL = "https://curso-vue-29a41-default-rtdb.firebaseio.com/"
+// axios.defaults.headers.commom['Authorization'] = 'abc123'
+// axios.defaults.headers.get['Accepts'] = 'application/json'
+
+Vue.use({
+    install(Vue){
+       // Vue.prototype.$http = axios 
+       Vue.prototype.$http = axios.create({
+           baseURL: 'https://curso-vue-29a41-default-rtdb.firebaseio.com/',
+           headers: { 
+                "Authorization": "abc123"
+           }
+       })
+
+       Vue.prototype.$http.interceptors.request.use(config => {   // INTERCEPTANDO A REQUISIÇÃO
+
+            console.log(config.method)
+            // if(config.method == 'post') {
+            //     config.method = 'put'
+            // }
+            return config
+       }, error => Promise.reject(error))
+
+       Vue.prototype.$http.interceptors.response.use(resp => {    // INTERCEPTANDO A RESPOSTA
+
+        //    const array = []
+        //    for(let chave in resp.data){
+        //       array.push({ id: chave, ...resp.data[chave] }) 
+        //    } 
+
+        //    resp.data = array
+           return resp
+       }, error => Promise.reject(error))
+    }
+})
